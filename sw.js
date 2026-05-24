fea7515effad88e963aa45bd7728a191d0d6a020
13san <91998034+13san@users.noreply.github.com>
2026-05-25 01:18:26 +0900

    Simplify service worker fetch event handling
    Removed caching logic and simplified fetch event handling.

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = "time-app-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
